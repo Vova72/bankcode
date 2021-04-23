331dbc2fe8c97878b7cc9b88ae71d186cab1dbf9
Vova <vovarusskih72@gmail.com>
2021-04-23 22:52:29 +0300

    make it better

--- src/main/java/com/vovarusskih72/bankcode/BankcodeApplication.java
+++ src/main/java/com/vovarusskih72/bankcode/BankcodeApplication.java
@@ -2,12 +2,14 @@ package com.vovarusskih72.bankcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Properties;
 
 @SpringBootApplication
 public class BankcodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankcodeApplication.class, args);
+        System.setProperty("java.version", "13");
     }
 
 }
